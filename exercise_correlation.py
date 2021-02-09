cec8ca5778e62231b27d14d0cabee7dd4c706646
faspeee <fabian.aspeeencina@studio.unibo.it>
2021-02-09 14:09:02 +0100

    pca outlier

--- exercise_correlation.py
+++ exercise_correlation.py
@@ -6,6 +6,8 @@ import pre_processing as pre
 import graphic as gr
 import download_file as dl
  
+from pca import pca
+ 
 data = dl.read_file() 
 wind_turbine = data[data['Wind_turbine_name'] =='R80721']
 wind_turbine = pre.extract_value_avg(wind_turbine.copy())
@@ -22,13 +24,36 @@ result=result.astype(float)
 wind_turbine = pre.fill_NaN_values(wind_turbine.copy())
 y_train =pre.select_y(wind_turbine)
 wind_turbine = wind_turbine.drop('Rbt_avg',axis=1)  
+ 
+
 #-----------------------------------FILLNaNVALUES AND SELECT TARGET----------------------------------
 
 #-----------------------------------SPLIT AND STANDARIZE DATA ---------------------------------------
-x_train, x_test, y_train, y_test = pre.train_test_split(wind_turbine,y_train) 
+x_train, x_test, y_train, y_test = pre.train_test_split(wind_turbine,y_train)  
 x_train = pre.standardize(x_train)
 x_test = pre.standardize(x_test)  
 #-----------------------------------SPLIT AND STANDARIZE DATA ---------------------------------------
+#-----------------------------------VIEW OUTLIER --------------------------------------------------
+
+# Fit transform verify because is not completely correctly
+model = pca(alpha=0.05, n_std=3)
+out = model.fit_transform(x_train) 
+display(out)
+print(out['outliers'])
+model.biplot(legend=True, SPE=True, hotellingt2=True)
+model.biplot3d(legend=True, SPE=True, hotellingt2=True)
+
+# Create only the scatter plots
+model.scatter(legend=True, SPE=True, hotellingt2=True)
+model.scatter3d(legend=True, SPE=True, hotellingt2=True)
+
+#ANOTHER WAY TO SEEN OUTLIER
+import pca
+outliers_hot = pca.hotellingsT2(out['PC'].values, alpha=0.05)
+outliers_spe = pca.spe_dmodx(out['PC'].values, n_std=2) 
+#-----------------------------------VIEW OUTLIER --------------------------------------------------
+
+
 #-----------------------------------PLOT ORIGINAL AND STANDAR DATA ---------------------------------
 ba_avg_data_graph = wind_turbine.Ba_avg.head(300) 
  
